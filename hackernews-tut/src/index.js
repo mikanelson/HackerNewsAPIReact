081a675372cab398cbd1bd589faaba137cdea654
mn <mn>
2020-11-09 14:39:49 -0500

    Working API base

--- hackernews-tut/src/index.js
+++ hackernews-tut/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {App} from './App';
-import * as serviceWorker from './ServiceWorker';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 render(<App />, document.getElementById('root'));
 
