3ce81460c1f66c1af4c3f97f20497e59117c3522
mn <mn>
2020-11-09 20:20:20 -0500

    Site styled

--- hackernews-tut/src/containers/StoriesContainer.js
+++ hackernews-tut/src/containers/StoriesContainer.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {getStoryIds} from '../services/hnApi';
 import {Story} from '../components/Story';
+import {GlobalStyle, StoriesContainerWrapper} from '../styles/StoriesContainerStyles';
+
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
@@ -11,8 +13,11 @@ export const StoriesContainer = () => {
 
   return (
     <>
-      <h1>Hacker News Stories</h1>
-      {storyIds.map(storyId => <Story key={storyId} storyId={storyId} />)};
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map(storyId => <Story key={storyId} storyId={storyId} />)};
+      </StoriesContainerWrapper>
     </>
   );
 }
