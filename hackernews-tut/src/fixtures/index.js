4d53148b84b8d056a72e2a7666b2f2d3292023e5
mn <mn>
2020-11-09 21:27:42 -0500

    Basic testing added

---
+++ hackernews-tut/src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+  by: 'Karl Hadwen',
+  id: 1,
+  time: 1567209822,
+  title: 'Tarnished: Google Responds',
+  url: 'https://www.tomshardware.com/news/google-responds-blink-chrome-vulnerability,40281.html',
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined,
+};
